c10444b83f5c7621948fefa764016973b831d459
Aisha Dahir <AishaDahir75@gmail.com>
2021-03-24 13:16:35 +0000

    Add workload trend & add arg parse

--- plots.py
+++ plots.py
@@ -1,18 +1,37 @@
 # Plot figures from helpdesk 
-
 import pandas as pd
 import numpy
 import matplotlib.pyplot as plt
 import datetime
+import os
+from plotnine import *
+import argparse
 
-data = pd.read_csv('/home/aisha/Documents/Projects/Binfx_Service_Reports/2101/Created_requests_Summary_2021-01-01_to_2021-01-31_by_day.csv', sep=',')
-data2 = pd.read_csv('/home/aisha/Documents/Projects/Binfx_Service_Reports/2101/Requests_completed_Summary_2021-01-01_to_2021-01-31_by_day.csv', sep=',')
+# arguements to point data folder containing csv files (2101 and alldata)
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-Total_tickets = data.sum(axis=1)
-Total_tickets = numpy.sum(Total_tickets)
-print(Total_tickets)
+    parser.add_argument(
+        '-m', '--month_data_path',
+        help='path to the month data folder',
+        required=True
+        )
+        
+    parser.add_argument(
+        '-a', '--all_data_path',
+        help='path to the all data folder',
+        required=True
+        )
+    
+    parser.add_argument(
+        '-d', '--date',
+        help='Date in short hand M Y e.g Jan 2021',
+        required=True
+        )
+    
+    args = parser.parse_args()
 
-date = 'Jan 2021'
+    return args
 
 def Plot_CreatedTicket(data, date):
     """Plots the number of tickets raised/created 
@@ -69,30 +88,66 @@ def Plot_CreatedVSCompletedTicket(data, data2, date):
     plot = Sum_ticketstypes.plot.bar(rot=0, title=date, color=['blue','darkgreen'])
     plot.figure.savefig("Numer_of_Tickets_Created_vs_Completed.png",  bbox_inches = "tight") # gets plot and saves it to png
 
-Plot_CreatedTicket(data, 'Jan 2021')
-Plot_CompletedTicket(data2, 'Jan 2021')
-Plot_CreatedVSCompletedTicket(data,data2, 'Jan 2021')
+def Plot_Workload(all_data, path):
+    all_tickets = all_data.sum(axis=1)
+    all_tickets = pd.DataFrame(all_tickets)
+    all_tickets.columns = ['Tickets raised']
+    # convert the date to short format
+    dates_list = list(all_data.index)
+    dates_list = [datetime.datetime.strptime(i, "%B %Y") for i in dates_list]
+    dates_list = [i.strftime("%b %Y") for i in dates_list]
+    dates = pd.DataFrame({'Dates': dates_list})
+    all_tickets.index = dates.Dates # make index with new date format
+    all_tickets = all_tickets.reset_index() # make index into a sep column 
+    all_tickets.columns = [ 'Dates', 'Tickets raised']
+    # Keep the order the dates are in at the moment, so its not alphabetical
+    all_tickets.sort_values(by='Dates').reset_index(drop = True)
+    all_tickets['Dates'] = pd.Categorical(all_tickets.Dates, categories=pd.unique(all_tickets.Dates))
+    # Plot dot and join lines (group = 1 helps points be joined)
+    plot = (ggplot(all_tickets, aes(x = 'Dates', y = 'Tickets raised', group = 1))
+            + geom_point()
+            + geom_line()
+            + labs(x = "", y='Tickets raised', title = "Workload trend")
+            + theme_classic()
+            + theme(text=element_text(family='DejaVu Sans',
+                                    size=10),
+                    axis_title=element_text(face='bold'),
+                    axis_text=element_text(face='italic'),
+                    plot_title=element_text(face='bold',
+                                            size=12)))
+    plot.save(path + '/Workload.png', height=6, width=len(dates_list)+2)
 
-###workload trend
+## Run functions
+def main():
 
-All_created = pd.read_csv('/home/aisha/Documents/Projects/Binfx_Service_Reports/AllData/Created_requests_Summary_2020-03-01_to_2021-02-22_by_month.csv', 
-                            sep = ',', index_col=0)
-#def Plot_Workload(data):
-data = All_created
-data2 = data.sum(axis=1)
-data2.columns = ['Number of tickets raised']
-x = pd.Index.tolist(data2.index)
-x2 = [datetime.datetime.strptime(i, "%B %Y") for i in x]
-x = [i.strftime("%b %Y") for i in x2]
-y = pd.Index.tolist(data2)
-fig, ax = plt.subplots()
-plt.figure(figsize=(15,8))
-xs = range(len(x))
-plt.axes(ax[0])
-plt.plot(xs, y)
-plt.xticks(xs, x)
-plt.tight_layout()
-plt.title("Workload", weight='bold')
-plt.savefig("Cumulative_workload.png",  bbox_inches = "tight") 
+    current_path = os.getcwd()
+    args = parse_args()
 
-#Plot_Workload(All_created)
+    # Read in this months created data
+    data_path = current_path + '/' + args.month_data_path
+    print(data_path)
+
+    # created csv
+    data_file =  [f for f in os.listdir(data_path) if f.startswith('Created_request')]
+    print(data_file[0])
+    created_data = pd.read_csv(data_path + '/' + data_file[0], sep=',')
+    # resolved csv
+    data_file =  [f for f in os.listdir(data_path) if f.startswith('Requests_completed')]
+    print(data_file[0])
+    resolved_data = pd.read_csv(data_path + '/' + data_file[0], sep=',')
+
+    # Read in all of data generated on helpdesk
+    all_data_path = current_path + '/' + 'data/all_data'
+    all_data_file =  [f for f in os.listdir(all_data_path) if f.startswith('Created_request')]
+    print(all_data_file[0])
+    all_data = pd.read_csv(all_data_path + '/' + all_data_file[0], sep=',', index_col=0)
+
+    date = args.date
+    Plot_CreatedTicket(created_data, date)
+    Plot_CompletedTicket(resolved_data, date)
+    Plot_CreatedVSCompletedTicket(created_data,resolved_data, date)
+    Plot_Workload(all_data, data_path)
+
+if __name__ == "__main__":
+
+    main()
